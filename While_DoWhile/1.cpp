c14ee9a63849ff4616d5871b4ba19b825aba9faa
Jared Diaz <00082426@uca.edu.sv>
2026-05-11 11:25:17 -0600

    update

---
+++ While_DoWhile/1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 0;
+    while (i < 11) {
+        cout << "i: " << i << endl;
+        i++;
+    }
+    
+    cout << "-----------------" << endl;
+    return 0;
+}
