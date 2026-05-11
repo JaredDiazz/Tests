d389d6281c6337dd6a4d787f91753b8605a943e4
Jared Diaz <00082426@uca.edu.sv>
2026-05-11 12:15:17 -0600

    update

---
+++ While_DoWhile/5.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+    int sum = 0;
+    int i = 1;
+
+    cout << "Enter a number: ";
+    cin >> number;
+
+    while (i <= number) {
+        if (i % 2 == 0) {
+            sum += i;
+        }
+        i++;
+    }
+
+    cout << "the sum of the even numbers from 1 to " << number 
+         << " is: " << sum << endl;
+    return 0;
+}
+    
