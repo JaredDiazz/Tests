0d3349822972091556db8040cd7259ff18b31239
Jared Diaz <00082426@uca.edu.sv>
2026-05-13 12:31:05 -0600

    update

--- If_Else_IfElse/18.cpp
+++ If_Else_IfElse/18.cpp
@@ -2,7 +2,15 @@
 using namespace std;
 
 int main() {
-    
-
+    string pass1, pass2;
+    cout << "Enter password: ";
+    cin >> pass1;
+    cout << "Re-enter password: ";
+    cin >> pass2;
+    if (pass1 == pass2) {
+        cout << "Password accepted." << endl;
+    } else {
+        cout << "Passwords do not match. Try again." << endl;
+    }
     return 0;
 }
